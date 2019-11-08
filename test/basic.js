4d733c963113cbf45532e782d0020ccffccb8225
Marius Obert <marius.obert@sap.com>
2019-11-08 16:03:09 +0100

    fix test

--- test/basic.js
+++ test/basic.js
@@ -4,26 +4,27 @@ const cds = require('@sap/cds');
 const express = require('express');
 
 const { PORT = 4004 } = process.env;
-const base_url = "http://localhost:4004/catalog"
+const base_url = 'http://localhost:4004/catalog';
+var server;
 
-describe("Checking the server process", function () {
+describe('Checking the server process', function () {
   this.timeout(12000);
 
   before(async () => {
     const app = express();
-    app.listen(PORT);
+    server = app.listen(PORT);
     await cds.connect(!cds.db && cds.env.requires.db || false);
     await cds.serve(undefined, { 'in-memory': true }).in(app);
   });
 
-  it("returns status code 200", function () {
+  it('returns status code 200', function () {
     return axios.get(base_url)
       .then(function (response) {
         assert.equal(200, response.status);
       });
   });
 
-  it("returns seven cities", function () {
+  it('returns seven cities', function () {
     return axios.get(`${base_url}/Cities`)
       .then(function (response) {
         assert.equal(6, response.data.value.length);
@@ -31,7 +32,7 @@ describe("Checking the server process", function () {
   });
 
   after(function () {
-    process.exit(); //exit manually to kill child process
+    cds.disconnect();
+    server.close()
   });
-
 });
